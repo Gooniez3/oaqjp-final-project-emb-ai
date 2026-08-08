888aff2ecb43b7b56a060214aa6237b5e8aae371
Gooniez3 <Gooniez3@users.noreply.github.com>
2026-08-08 08:00:28 -0400

    Add blank input error handling

--- server.py
+++ server.py
@@ -14,6 +14,9 @@ def emotion_analyzer():
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
 
+    if response["dominant_emotion"] is None:
+        return "Invalid text! Please try again!"
+
     return (
         "For the given statement, the system response is "
         f"'anger': {response['anger']}, "
